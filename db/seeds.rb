73e3fb7de2e00a84ff9ed5d898b58bf02e2f5276
Aaron Mednick <aaronjosephm@gmail.com>
2018-08-07 14:21:37 -0400

    Added some routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+# TODO: Write a seed to insert 100 posts in the database
+
+puts 'Creating 100 fake articles...'
+100.times do
+  article = Article.new(
+    title:    Faker::Lovecraft.tome,
+    content: "#{Faker::Lovecraft.paragraphs}"
+  )
+  article.save!
+end
+puts 'Finished!'
---
+++ test/controllers/articles_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  test "should get create" do
+    get articles_create_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get articles_show_url
+    assert_response :success
+  end
+
+end
